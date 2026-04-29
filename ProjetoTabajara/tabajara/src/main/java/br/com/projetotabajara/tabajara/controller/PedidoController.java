8adb261b490994fec9075d1da96b2ca49ef57a86
victor-hugo-de-alencar-morais <vham17.com.br@gmail.com>
2026-04-28 22:41:30 -0300

    feito o formulario do pedido e a respectiva controller

--- ProjetoTabajara/tabajara/src/main/java/br/com/projetotabajara/tabajara/controller/PedidoController.java
+++ ProjetoTabajara/tabajara/src/main/java/br/com/projetotabajara/tabajara/controller/PedidoController.java
@@ -1,9 +1,19 @@
 package br.com.projetotabajara.tabajara.controller;
 
+import java.util.List;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
 
+import br.com.projetotabajara.tabajara.entity.Pedido;
+import br.com.projetotabajara.tabajara.entity.Produto;
+import br.com.projetotabajara.tabajara.entity.Usuario;
 import br.com.projetotabajara.tabajara.service.PedidoService;
 import br.com.projetotabajara.tabajara.service.ProdutoService;
 import br.com.projetotabajara.tabajara.service.UsuarioService;
@@ -21,4 +31,24 @@ public class PedidoController {
     @Autowired
     private ProdutoService produtoService;
     
+    //endpoint para salvar o pedido (json usado pelo fatch)
+    @PostMapping
+    @ResponseBody
+    public Pedido salvarPedido(@RequestBody Pedido pedido){
+        return pedidoService.salvarPedido(pedido);
+    }
+
+    @GetMapping("/criar")
+    public String criarForm(Model model){
+        model.addAttribute("pedido", new Pedido());
+
+        List<Usuario> usuarios = usuarioService.findAll();
+        model.addAttribute("usuarios", usuarios);
+
+        List<Produto> produtos = produtoService.findAll();
+        model.addAttribute("produtos", produtos); 
+        return "pedido/formularioPedido";
+    }
+
+
 }
